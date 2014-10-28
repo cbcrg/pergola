2a42d5bfbce552830911dbb751e4efe3d96e3af2
Jose Espinosa <kadomu@gmail.com>
2014-10-28 13:31:35 +0100

    Initial setup of the package

---
+++ pergola/__init__.py
@@ -0,0 +1,2 @@
+def readme():
+    print "A library to visualize behavioral data in a genome browser"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pergola',
+      version='0.1',
+      description='A library to visualize behavioral data in a genome browser',
+      url='http://github.com/cbcrg/pergola',
+      author='Jose Espinosa-Carrasco',
+      author_email='espinosacarrascoj@gmail.com',
+      license='GNU general public license version 3',
+      packages=['pergola'],
+      zip_safe=False)
