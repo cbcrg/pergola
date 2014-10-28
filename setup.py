20832a84bbd09be6cdf03cfaf99f3dacf3fe4e31
Jose Espinosa <kadomu@gmail.com>
2014-10-28 16:10:50 +0100

    setup.py corrected

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(name='pergola',
       version='0.1',
@@ -6,6 +6,6 @@ setup(name='pergola',
       url='http://github.com/cbcrg/pergola',
       author='Jose Espinosa-Carrasco',
       author_email='espinosacarrascoj@gmail.com',
-      license='GNU general public license version 3',
+      license='GNU General Public License 3.0',
       packages=['pergola'],
       zip_safe=False)
