40dfa54f3e83bdcdf64e4237cbdb2addabce96c2
Jose Espinosa <kadomu@gmail.com>
2014-10-28 22:08:34 +0100

    __init__.py structure

--- pergola/__init__.py
+++ pergola/__init__.py
@@ -1,2 +1,23 @@
-def readme():
-    print "A library to visualize behavioral data in a genome browser"
+"""
+===========
+Description
+===========
+
+pegola is a set of python modules for transforming your data in data visualizable
+in common genome browser
+
+============
+Installation
+============
+
+=======
+License
+=======
+
+
+"""
+
+__version__ = '0.0.1'
+
+# def readme():
+#     print "A library to visualize behavioral data in a genome browser"
