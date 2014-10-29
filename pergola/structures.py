3895344ad66e7b525a4e440cc96e81e5f548e7a5
Jose Espinosa <kadomu@gmail.com>
2014-10-29 17:10:35 +0100

    adding _check_delimiter()

--- pergola/structures.py
+++ pergola/structures.py
@@ -1,21 +1,49 @@
 class IntData: 
     """
-    Generic class for data
-    Possible thinks to implement
+    Generic class for input data
     
     .. attribute:: path
-    .. attribute:: delimiter
-    .. attribute:: header
-    .. attribute:: fieldsB
-    .. attribute:: fieldsG
-    .. attribute:: data
-    .. attribute:: min
-    .. attribute:: max
-    .. attribute:: dataTypes
-    .. attribute:: tracks
+       Name of path to a csv/tab input file
+    
+    .. attribute:: separator
+       Character use to separate values of the same record in file (default "\t").
     
     list with the behavioral fields corresponding each column in the original file
+    
+    :return: IntData object
+    
      
     """
     def __init__(self, path, ontology_dict, **kwargs):
         self.path = check_path(path)
+        self.delimiter = self._check_delimiter(self.path, kwargs.get('delimiter',"\t"))
+        
+    def _check_delimiter (self, path, delimiter):
+        """ 
+        Check whether the delimiter works, if delimiter is not set then tries ' ', '\t' and ';'
+         
+        :param path: (str) name of path to a behavioral file in the form of a csv file
+        :param delimiter: (str) delimiter used in the file ("\t", ";", " ") 
+         
+        """
+        
+        if delimiter is None: 
+            raise ValueError("Delimiter must be set \'%s\'"%(delimiter))
+        
+        self.inFile  = open(path, "rb")
+        
+        for row in self.inFile:            
+            if row.count(delimiter) >= 1: break
+            else: raise ValueError("Input delimiter does not correspond to delimiter found in file \'%s\'"%(self.delimiter))
+            
+            if row.count(" ") >= 1:
+                self.delimiter = " "
+                break
+            if row.count("\t") >= 1:
+                self.delimiter = "\t"
+                break
+            if row.count(";") >= 1:
+                self.delimiter = "\t"
+                break      
+            
+        return delimiter
