52529216d8003401bf2db27ae132ea7f9fac846a
Jose Espinosa <kadomu@gmail.com>
2014-10-29 17:22:45 +0100

    _check_delimiter if not suitable delimiter found raise exception

--- pergola/structures.py
+++ pergola/structures.py
@@ -5,7 +5,7 @@ class IntData:
     .. attribute:: path
        Name of path to a csv/tab input file
     
-    .. attribute:: separator
+    .. attribute:: delimiter
        Character use to separate values of the same record in file (default "\t").
     
     list with the behavioral fields corresponding each column in the original file
@@ -16,20 +16,17 @@ class IntData:
     """
     def __init__(self, path, ontology_dict, **kwargs):
         self.path = check_path(path)
-        self.delimiter = self._check_delimiter(self.path, kwargs.get('delimiter',"\t"))
+        self.delimiter = self._check_delimiter(self.path, kwargs.get('delimiter', None))
         
     def _check_delimiter (self, path, delimiter):
         """ 
         Check whether the delimiter works, if delimiter is not set then tries ' ', '\t' and ';'
          
         :param path: (str) name of path to a behavioral file in the form of a csv file
-        :param delimiter: (str) delimiter used in the file ("\t", ";", " ") 
+        :param delimiter: (str) delimiter used in the file ("tab", ";", "space") 
          
         """
-        
-        if delimiter is None: 
-            raise ValueError("Delimiter must be set \'%s\'"%(delimiter))
-        
+                        
         self.inFile  = open(path, "rb")
         
         for row in self.inFile:            
@@ -45,5 +42,8 @@ class IntData:
             if row.count(";") >= 1:
                 self.delimiter = "\t"
                 break      
+        
+        if delimiter is None: 
+            raise ValueError("Delimiter must be set \'%s\'"%(delimiter))
             
         return delimiter
