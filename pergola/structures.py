8170f3629160dfc7a6cbffd7f63f2b77b6905490
Jose Espinosa <kadomu@gmail.com>
2014-10-30 09:46:34 +0100

    Adding _set_fields_b and in some new formats for sphinx documentation

--- pergola/structures.py
+++ pergola/structures.py
@@ -13,7 +13,9 @@ class IntData:
        * `False` if there is no header. Fields should the be provided using fields param
        * `True` if the file have a header line with names. This names should match names in ontology_dict (default).
     
-    list with the behavioral fields corresponding each column in the original file
+    .. attribute:: fieldsB
+        List with the behavioral fields corresponding each column in the file
+        
     
     :return: IntData object
     
@@ -23,13 +25,14 @@ class IntData:
         self.path = check_path(path)
         self.delimiter = self._check_delimiter(self.path, kwargs.get('delimiter', None))
         self.header = kwargs.get('header',True)
+        self.fieldsB = self._set_fields_b(kwargs.get('fields'))
         
     def _check_delimiter (self, path, delimiter):
         """ 
         Check whether the delimiter works, if delimiter is not set then tries ' ', '\t' and ';'
          
-        :param path: (str) name of path to a behavioral file in the form of a csv file
-        :param delimiter: (str) delimiter used in the file ("tab", ";", "space") 
+        :param path: :py:func:`str` name of path to a behavioral file in the form of a csv file
+        :param delimiter: :py:func:`str` delimiter used in the file ("tab", ";", "space") 
          
         """
                         
@@ -53,3 +56,59 @@ class IntData:
             raise ValueError("Delimiter must be set \'%s\'"%(delimiter))
             
         return delimiter
+    
+    def _set_fields_b(self, fields=None):
+        """
+        Reading the behavioral fields from the header file or otherwise setting  
+        the fields to numeric values corresponding the column index starting at 0
+        
+        :param None fields: :py:func:`list` with the behavioral fields corresponding each column in the file
+        
+        :return: a list with the behavioral fields
+            
+        """ 
+        self.inFile  = open(self.path, "rb")
+        self.reader = csv.reader(self.inFile, delimiter=self.delimiter)       
+        
+        if self.header:            
+            header = self.reader.next()
+            first_r = self.reader.next()
+            
+            if len(header) != len(first_r):
+                raise ValueError("Number of fields in header '%d' does not match number of fields in first row '%d'" 
+                                 % (len(header), len(first_r)))
+            
+            if fields:
+                if len(fields) > len(first_r):
+                    raise ValueError("Input field list \"%s\" is longer than totals fields available in file \'%s\'" 
+                                     % ("\",\"".join(fields), len(first_r)))
+                
+                if not all(field in header for field in fields):
+                    raise ValueError("Input field list \"%s\" has items not present in file header \'%s " 
+                                     '\n'
+                                     "Also make sure you don't need to set header=False"
+                                     % ("\",\"".join(fields), "\",\"".join(header)))
+                
+                fieldsB = fields
+                     
+            else:       
+                fieldsB = [header[0].strip('# ')]+header[1:]        
+        else:
+            first_r = self.reader.next()
+            
+            if fields:
+                if len(fields) > len(first_r):
+                    raise ValueError("Input field list \"%s\" is longer than totals fields available in file \'%s\'" % ("\",\"".join(fields), len(first_r)))            
+                            
+                fieldsB = fields
+                
+                print ("WARNING: As header=False you col names set by fields will be consider to have the order "
+                        "you provided: %s" 
+                        %"\",\"".join(fields))                 
+            else:                                
+                raise ValueError ('File should have a header, otherwise you should set ' 
+                                  'an ordered list of columns names using fields')     
+                
+        self.inFile.close()
+        
+        return fieldsB
