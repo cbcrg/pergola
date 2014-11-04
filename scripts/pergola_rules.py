fcfbf8a8901b944df4ed5a3ce530f9da7a4fef98
Jose Espinosa <kadomu@gmail.com>
2014-11-04 10:48:00 +0100

    --track_actions -d implemented in pergola_rules.py

--- scripts/pergola_rules.py
+++ scripts/pergola_rules.py
@@ -14,6 +14,7 @@ from re import match
 import os
 
 _dt_act_options = ['all', 'one_per_channel']
+_tr_act_options = ['split_all', 'join_all', 'join_odd', 'join_even', 'join_list'] 
 
 def main():
     parser = ArgumentParser(description = 'Script to transform behavioral data into GB readable data')
@@ -26,7 +27,10 @@ def main():
     parser.add_argument('-l','--list', required=False, metavar="LIST_OF_TRACKS", type=str, nargs='+',
                         help='Numeric list of tracks to be joined in a single genomic like file')### string allowed as some tracks could be named as: track_1, track2....
     parser.add_argument('-r','--range', required=False, type=parse_num_range,
-                        help='Numeric range of tracks to be joined in a single genomic like file',)
+                        help='Numeric range of tracks to be joined in a single genomic like file')
+    parser.add_argument('-a','--track_actions', required=False, choices=_tr_act_options,
+                        help='Option of action with tracks selected, split_all, join_all,' + \
+                             ' join_odd, join_even, join_range or join_list')
     parser.add_argument('-d','--dataTypes_actions', required=False, choices=_dt_act_options,
                         help='Unique values of dataTypes field should be dumped on' + \
                              ' different data structures or not')
@@ -60,6 +64,14 @@ def main():
     
     if tracks2merge: print >> stderr, "Tracks to join are: ", tracks2merge
     
+    # Handling argument track actions
+    if tracks2merge and args.track_actions:
+        raise ValueError ("Options --list -l or --range -r are incompatible with " \
+                          "--track_actions -a, please change your options")
+    
+    track_act = args.track_actions
+    print >> stderr, "@@@Pergola_rules.py Track actions are: ", track_act
+    
     # Handling argument dataTypes actions
     dataTypes_act = args.dataTypes_actions
     print >> stderr, "@@@Pergola_rules.py dataTypes actions are: ", dataTypes_act
@@ -68,9 +80,12 @@ def main():
     
     
     
+    
     ################
     # Reading data
     intData = structures.IntData(path, ontology_dict=config_file_dict.correspondence)
+    
+    tracks2merge = read_track_actions(tracks=intData.tracks, track_action=track_act)
 #     print "____________",intData.tracks
 #     print "::::::::", intData.data
     structures.write_chr (intData)
@@ -109,6 +124,34 @@ def parse_num_range(string):
     set_range=set(['{0}'.format(t) for t in list_range]) #str because track can be set in the form of track_1 for instance
     
     return set_range
+
+def read_track_actions (tracks, track_action = "split_all"):
+    """ 
+    Read track actions and returns a set with the tracks to be joined
+    
+    :param tracks: (set) of tracks to which track_action should be applied set([1,2])
+    :param track_action: (str) option to join tracks (join_all, split_all, join_odd, join_evens) 
+    """
+    
+    if track_action not in _tr_act_options:
+        raise ValueError("Track_action \'%s\' not allowed. Possible values are %s"%(track_action,', '.join(['{}'.format(m) for m in tr_act_options])))
+    
+    tracks2merge = ""
+    print >> stderr, "Tracks to merge are: ", ",".join(tracks2merge)
+    if track_action == "join_all":
+        tracks2merge = tracks
+    elif track_action == 'join_odd':
+        tracks2merge = set([t for t in tracks if int(t) % 2])
+    elif track_action == 'join_even':
+        tracks2merge = set([t for t in tracks if not int(t) % 2])
+    else:
+        tracks2merge = ""
+    print >> stderr,"Tracks to merge are: ", ",".join("'{0}'".format(t) for t in tracks2merge)
+       
+    if not tracks2merge:
+        print >> stderr,("No track action applied as track actions \'%s\' can not be applied to list of tracks provided \'%s\'"%(track_action, " ".join(tracks)))
+        
+    return (tracks2merge)
 
 if __name__ == '__main__':
     exit(main())
