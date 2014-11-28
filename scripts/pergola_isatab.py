c8934650b502b397549c4077d95f4643378e43bd
JoseEspinosa <kadomu@gmail.com>
2014-11-28 17:49:14 +0100

    Typo in pergola_isatab import

--- scripts/pergola_isatab.py
+++ scripts/pergola_isatab.py
@@ -6,3 +6,23 @@
 Script to run pergola from the command line using isatab format
 """
 
+from pergola import parsers
+from pergola  import intervals
+from pergola  import mapping
+from argparse import ArgumentParser, ArgumentTypeError
+from sys      import stderr
+import os
+
+def main():
+    parser = ArgumentParser(parents=[parsers.parser])
+    
+    args = parser.parse_args()
+    
+    print >> stderr, "@@@Pergola_isatab.py: Input file: %s" % args.input 
+    print >> stderr, "@@@Pergola_isatab.py: Configuration file: %s" % args.ontology_file
+    print >> stderr, "@@@Pergola_isatab.py: Selected tracks are: ", args.tracks
+
+
+
+if __name__ == '__main__':
+    exit(main())
