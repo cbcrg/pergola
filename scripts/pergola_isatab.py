f8bf5cf2d17b57a11d100e4ead985ba089067f38
JoseEspinosa <kadomu@gmail.com>
2014-11-29 20:38:57 +0100

    pergola_isatab.py reads from isatab data folder

--- scripts/pergola_isatab.py
+++ scripts/pergola_isatab.py
@@ -12,6 +12,7 @@ from pergola  import mapping
 from argparse import ArgumentParser, ArgumentTypeError
 from sys      import stderr
 import os
+from bcbio import isatab
 
 def main():
     parser = ArgumentParser(parents=[parsers.parser])
@@ -22,9 +23,16 @@ def main():
     print >> stderr, "@@@Pergola_isatab.py: Configuration file: %s" % args.ontology_file
     print >> stderr, "@@@Pergola_isatab.py: Selected tracks are: ", args.tracks
 
-#Make a function that retrieves files in isatab table and return it as a dictionary
-    
-#hacer una funcion que recoja todos los archivos del isatab y los meta en un diccionario
+    #Make a function that retrieves files in isatab table and return it as a dictionary
+    isatab_ref = "/home/kadomu/Dropbox/isaTabTools/ISAcreator-1.7.7/isatab files/test2_int2GB"
+
+    # I have to check whether when a isatab folder is given if it is actually a folder or a file
+    # difference with -i
+    if os.path.isdir(isatab_ref):
+        print "Is a directory"
+        print "Directory is", args.input 
+        #print "directory is ", args.input
+#It might be interesting to implement a append option
 
 if __name__ == '__main__':
     exit(main())
