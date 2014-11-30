891469f3636eddfe8a135ac66aa6b51041b33ca9
JoseEspinosa <kadomu@gmail.com>
2014-11-30 13:14:17 +0100

    Reading files inside isatab folder corresponding to assay

--- scripts/pergola_isatab.py
+++ scripts/pergola_isatab.py
@@ -24,7 +24,7 @@ def main():
     print >> stderr, "@@@Pergola_isatab.py: Selected tracks are: ", args.tracks
 
     #Make a function that retrieves files in isatab table and return it as a dictionary
-    isatab_ref = "/home/kadomu/Dropbox/isaTabTools/ISAcreator-1.7.7/isatab files/test2_int2GB"
+#     isatab_ref = "/home/kadomu/Dropbox/isaTabTools/ISAcreator-1.7.7/isatab files/test2_int2GB"
 
     # I have to check whether when a isatab folder is given if it is actually a folder or a file
     # difference with -i
@@ -33,9 +33,31 @@ def main():
     
     rec = isatab.parse(args.input)
     study= rec.studies[0]
-    print study.nodes.keys() 
+#     print study.nodes.keys() 
+    
+    #Sample name are the key shared by both study and assay
+    
+    for i in rec.studies:
+#         print "studies are", i
+        
+        for j in i.assays:
+            print "assays are:", j
+            for file in j.nodes.keys():
+                print "file is :", file
+#     for i in rec.assays:
+#         print "assays", i    
     
     
+    
+#     for k in study.nodes.keys() :
+#         print k
+#         node=study.nodes[k]
+#         print node.assays.keys()
+#         
+    nodes=study.nodes
+    
+#     for node in nodes:
+#         print "node------------", node ["Raw Data File"]
         #print "directory is ", args.input
 #It might be interesting to implement a append option
 
