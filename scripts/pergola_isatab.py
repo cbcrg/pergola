e3cd14f637715f53ba5b0af2e0e8890a2dda3314
JoseEspinosa <kadomu@gmail.com>
2014-12-01 10:01:54 +0100

    Reading options to pass them from pergola_isatab to calls of pergola_rules

--- scripts/pergola_isatab.py
+++ scripts/pergola_isatab.py
@@ -6,7 +6,7 @@
 Script to run pergola from the command line using isatab format
 """
 
-space = " "
+sp = " "
 
 from pergola import parsers
 from pergola  import intervals
@@ -24,7 +24,37 @@ def main():
     print >> stderr, "@@@Pergola_isatab.py: Input file: %s" % args.input 
     print >> stderr, "@@@Pergola_isatab.py: Configuration file: %s" % args.ontology_file
     print >> stderr, "@@@Pergola_isatab.py: Selected tracks are: ", args.tracks
-
+    
+#     print ("pergola_rules.py" + sp + "-i" + sp + file + sp + "-o" + sp + args.ontology_file + sp +
+#                        track_actions + sp + dataTypes_actions + 
+#                        sp + format + sp + relative_coord + sp + 
+#                        multiply_factor + sp + fields_read) 
+    
+    relative_coord = ""
+    track_actions = ""
+    dataTypes_actions = ""
+    format = ""
+    multiply_factor = ""
+    fields_read = ""
+    
+    if args.relative_coord:
+        relative_coord = "-e"
+        
+    if args.track_actions:
+        track_actions = "-a " + args.track_actions
+    
+    if args.dataTypes_actions:
+        dataTypes_actions = "-d " + args.dataTypes_actions
+    
+    if args.format:
+        format = "-f " + args.format
+    
+    if args.multiply_factor:
+        multiply_factor = "-f " + args.multiply_factor
+        
+    if args.fields_read: 
+        fields_read = "-m " + args.fields_read
+                
     #Make a function that retrieves files in isatab table and return it as a dictionary
 #     isatab_ref = "/home/kadomu/Dropbox/isaTabTools/ISAcreator-1.7.7/isatab files/test2_int2GB"
 
@@ -45,10 +75,36 @@ def main():
         for j in i.assays:
             #print "assays are:", j
             for file in j.nodes.keys():
-                print "file is :", file
+#                 print "file is :", file
                 print ("")
-                print ("pergola_rules.py" + space + "-i" + space + file + space + "-o" + space + args.ontology_file) 
+                print ("pergola_rules.py" + sp + "-i" + sp + file + sp + "-o" + sp + args.ontology_file + sp +
+                       track_actions + sp + dataTypes_actions + 
+                       sp + format + sp + relative_coord + sp + 
+                       multiply_factor + sp + fields_read) 
 #                 os.system("pergola_rules.py")
+
+
+# parser.add_argument('-a', '--track_actions', required=False, choices=_tr_act_options,
+#                     help='Option of action with tracks selected, split_all, join_all,' + \
+#                          ' join_odd, join_even, join_range or join_list')
+# parser.add_argument('-d', '--dataTypes_actions', required=False, choices=_dt_act_options,
+#                     help='Unique values of dataTypes field should be dumped on' + \
+#                          ' different data structures or not')
+# parser.add_argument('-f', '--format', required=False, type=str, 
+#                     help='Write file output format (bed or bedGraph)')
+# parser.add_argument('-e', '--relative_coord', required=False, action='store_true', 
+#                     default=False, help='Sets first timepoint' \
+#                     ' to 0 and make all the others relative to this timepoint')
+# parser.add_argument('-n', '--intervals_gen', required=False, action='store_true', 
+#                     default=False, help='Set startChrom and endChrom from just a timepoint in the file' \
+#                                         'using field set as startChrom')
+# parser.add_argument('-m', '--multiply_factor', metavar='N', type=int, required=False,
+#                     help='Multiplies value in dataValue by the given value')
+# parser.add_argument('-s', '--fields_read', metavar='FIELDS2READ', type=str, nargs='+',
+#                     help='List of fields to read from input file')
+
+
+
 #     for i in rec.assays:
 #         print "assays", i    
     
