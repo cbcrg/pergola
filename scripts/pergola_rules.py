31e8ed07e10c651de7543e48eeb22700e036a27a
JoseEspinosa <kadomu@gmail.com>
2014-12-01 11:11:34 +0100

    Cleaning code

--- scripts/pergola_rules.py
+++ scripts/pergola_rules.py
@@ -172,19 +172,7 @@ def main():
                                       
                                     
                                     
-                                    
-                                    
-# def parse_num_range(string):
-#     m = match(r'(\d+)(?:-(\d+))?$', string)
-# 
-#     if not m:
-#         raise ArgumentTypeError("'" + string + "' is not a range of number. Expected '0-5' or '2'.")
-#     start = m.group(1)
-#     end = m.group(2) or start
-#     list_range=list(range(int(start,10), int(end,10)+1))
-#     set_range=set(['{0}'.format(t) for t in list_range]) #str because track can be set in the form of track_1 for instance
-#     
-#     return set_range
+                               
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     exit(main())
