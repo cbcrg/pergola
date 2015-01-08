28aa9b5603bf15421093c5de51eb171184ea5ee1
Jose Espinosa <kadomu@gmail.com>
2015-01-08 22:55:42 +0100

    Adding examples of how to use venn_mpl.py

--- test/test_pybedtools.py
+++ test/test_pybedtools.py
@@ -15,3 +15,11 @@ c.saveas("/home/kadomu/git/pergola/a_inter_b.bed")
 (a-b-c).count()  # unique to a
 (a+b-c).count()  # in a and b, not c
 (a+b+c).count()  # common to all
+
+#venn diagram script 
+venn_mpl.py -a food.bed -b water.bed -c i.bed
+
+# BEDTools examples
+
+intersectBed -a tr_1_dt_food_sc.bed -b tr_1_dt_water.bed > food_sc_waterBedTools.bed
+closestBed -a tr_1_dt_food_sc.bed -b tr_1_dt_water.bed > food_sc_waterBedToolsClosest.bed
