19738727383f31e10514134f98d31c0581dc43a8
Jose Espinosa <kadomu@gmail.com>
2015-02-06 18:14:06 +0100

    Adding a parameter to set data type in the csv file

--- test/jaaba2pergola_dev.py
+++ test/jaaba2pergola_dev.py
@@ -17,7 +17,7 @@ from os.path import join, exists
 
 jaaba_data = io.loadmat('/Users/jespinosa/JAABA_MAC_0.5.1/sampledata/Chase1_TrpA_Rig1Plate15BowlA_20120404T141155/scores_chase.mat')
 
-def jaaba_scores_to_csv (input_file, name_file="JAABA_scores", mode="w", delimiter="\t", path_w=None, norm=False):
+def jaaba_scores_to_csv (input_file, name_file="JAABA_scores", mode="w", delimiter="\t", path_w=None, norm=False, data_type="a"):
     """   
     Creates a csv file from a scores file produced using JAABA and in matlab
     format
@@ -28,6 +28,8 @@ def jaaba_scores_to_csv (input_file, name_file="JAABA_scores", mode="w", delimit
     :param None path_w: :py:func:`str` path to dump the files, by default None
     :param False norm: set whether data should be normalize (-1,1) using normalization
         factor contained in the file
+    :param data_type: :py:func:`str` data type in the file "behavior" e.g. chase
+    
     TODO make a dump mode to save the data directly in pergola format
     I will need then the correspondence file, or that input functions or pergola
     allow object and not always files 
@@ -35,7 +37,7 @@ def jaaba_scores_to_csv (input_file, name_file="JAABA_scores", mode="w", delimit
     """
     
     path = ""
-    header = ["animal", "startTime", "endTime", "value"]
+    header = ["animal", "startTime", "endTime", "value", "dataType"]
     if not path_w: 
         path = getcwd()
         print >>stderr, 'CSV file will be dump into \"%s\" ' \
@@ -90,10 +92,11 @@ def jaaba_scores_to_csv (input_file, name_file="JAABA_scores", mode="w", delimit
             # that is why I substract one to the end_time
             # In fact in the graphical interface it starts at start_time - 0.5 and ends in 
             # end_time - 0.5
-            scoreFile.write("\t".join('{}'.format(v) for v in [idx_animal+1, start_time, end_time -1, mean_score]) + "\n") 
+            scoreFile.write("\t".join('{}'.format(v) for v in [idx_animal+1, start_time, end_time -1, mean_score, data_type]) + "\n") 
 
     scoreFile.close()
     
 # jaaba_scores_to_csv (input_file='/Users/jespinosa/JAABA_MAC_0.5.1/sampledata/Chase1_TrpA_Rig1Plate15BowlA_20120404T141155/scores_chase.mat', path_w='/Users/jespinosa/git/pergola/test')
-jaaba_scores_to_csv (input_file='/Users/jespinosa/JAABA_MAC_0.5.1/sampledata_v0.5/Chase1_TrpA_Rig1Plate15BowlA_20120404T141155/scores_chase.mat', path_w='/Users/jespinosa/git/pergola/test', norm=True)
+jaaba_scores_to_csv (input_file='/Users/jespinosa/JAABA_MAC_0.5.1/sampledata_v0.5/Chase1_TrpA_Rig1Plate15BowlA_20120404T141155/scores_chase.mat', 
+                     path_w='/Users/jespinosa/git/pergola/test', norm=True, data_type="chase")
 exit ("You correctly transform your JAABA scores into a csv file (pergola readable)")
