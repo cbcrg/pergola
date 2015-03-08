85c83cd3bc3f1a6590949ac9a667c70abbf3e01c
Jose Espinosa <kadomu@gmail.com>
2015-03-08 23:32:46 +0100

    File to generate igv snapshot

--- test/igv_snapshot.sh
+++ test/igv_snapshot.sh
@@ -8,9 +8,15 @@ echo "snapshotDirectory $HOME/tmp/igv"
 sleep 2
 echo "goto chr1:1-1,780,869"
 sleep 2
-echo "snapshot"
+echo "snapshot region.svg"
 sleep 2
 echo "goto chr1:919,000-982,000"
 sleep 2
-echo "snapshot"
+echo "snapshot region_two.svg"
 sleep 2
+sleep 2
+echo "goto chr1:820,802-950,400"
+sleep 2
+echo "snapshot region_three.svg"
+sleep 2
+
