f02d646822d3f5d8b614e46e11f7faec10e10049
Jose Espinosa <kadomu@gmail.com>
2015-07-23 11:10:23 +0200

    Unittest function to check reading of correspondence file of tutorial

--- pergola/test/test_all.py
+++ pergola/test/test_all.py
@@ -5,9 +5,27 @@ unittest for pergola functions
 """
 
 import unittest
+from pergola import mapping
+from pergola import intervals
+from os import path
 
-def printTest ():
-    return ('This is a test')
+# Getting the path to test files
+PATH = path.abspath(path.split(path.realpath(__file__))[0])
 
+class TestTutorial(unittest.TestCase):
+    """
+    Testing that everything in the tutorial run smoothly        
+    """ 
+        
+    def test_ontology_info (self):
+        """
+        Testing that ontology file is correctly read
+        """
+        global exp1, exp2, exp3, exp4
+        exp_1 = mapping.OntologyInfo(PATH + "/feeding/tutorial/b2g.txt")
+        
+        self.assertEqual(exp_1.correspondence['EndT'], 'chromEnd')
+
+    
 if __name__ == '__main__':
     unittest.main()
