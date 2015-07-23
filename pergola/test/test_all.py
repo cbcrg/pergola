64e6276e55dcabf48489d8e8f1e2838486ed5db5
Jose Espinosa <kadomu@gmail.com>
2015-07-23 11:54:39 +0200

    Checking that IntData instance using tutorial data works properly

--- pergola/test/test_all.py
+++ pergola/test/test_all.py
@@ -17,21 +17,27 @@ class TestTutorial(unittest.TestCase):
     Testing that everything in the tutorial run smoothly        
     """ 
         
-    def test_ontology_info (self):
+    def test_correspondence_info (self):
         """
         Testing that ontology file is correctly read
         """
         global correspondence_tutorial, exp2, exp3, exp4
         correspondence_tutorial = mapping.OntologyInfo(PATH + "/feeding/tutorial/b2g.txt")
         
-        self.assertEqual(correspondence_tutorial.correspondence['EndT'], 'chromEnd')
+        msg_correspondence = "Corresponding tutorial file equivalences are not correct."
+        self.assertEqual(correspondence_tutorial.correspondence['EndT'], 'chromEnd', msg_correspondence)
 
     def test_read_int_data(self):
         """
         Testing the creation of intData object using tutorial data
         """ 
         global int_data_tutorial
+        
+        # Min value from tutorial file
+        min = 1335985200
+        msg_int_data_min= "Min value does not correspond to tutorial files."
         int_data_tutorial = intervals.IntData(PATH + "/feeding/tutorial/feedingBehavior_HF_mice.csv", ontology_dict=correspondence_tutorial.correspondence)
+        self.assertEqual(int_data_tutorial.min, min, msg_int_data_min) 
         
 if __name__ == '__main__':
     unittest.main()
