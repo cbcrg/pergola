e5ce395194e94ec9479262690e23e87d1f86ca50
Jose Espinosa <kadomu@gmail.com>
2015-08-10 18:22:00 +0200

    Changing layout of documentation and first time including everything in sphinx documentation

--- doc/conf.py
+++ doc/conf.py
@@ -106,7 +106,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -353,3 +354,9 @@ epub_exclude_files = ['search.html']
 
 # Link to python documentation
 intersphinx_mapping = {'python': ('http://docs.python.org/2', None)}
+
+## Adding nature theme with ribbon (forkme_nature) https://github.com/dirn/sphinx-forkme-nature
+sys.path.append(os.path.abspath('_themes'))
+
+html_theme_path = ['_themes']
+html_theme = 'forkme_nature'
