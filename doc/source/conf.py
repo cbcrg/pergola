d8832c71f923985818d0f3dba06a3d210bf11607
Jose Espinosa <kadomu@gmail.com>
2015-08-11 17:19:03 +0200

    Going back to nature theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -107,8 +107,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'# Theme_change
-#html_theme = 'nature'# Theme_change
+#html_theme = 'default'# Theme_change
+html_theme = 'nature'# Theme_change
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -360,5 +360,5 @@ intersphinx_mapping = {'python': ('http://docs.python.org/2', None)}
 sys.path.append(os.path.abspath('_themes'))
 
 html_theme_path = ['_themes']
-#html_theme = 'forkme_nature'# Theme_change
-html_theme = "sphinx_rtd_theme" # Theme_change
+html_theme = 'forkme_nature'# Theme_change
+#html_theme = "sphinx_rtd_theme" # Theme_change
