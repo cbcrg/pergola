0df7f2b28ffdfb21d66fda5b603d343fddf92973
Jose Espinosa <kadomu@gmail.com>
2016-01-22 12:10:55 +0100

    Correct implementation of merge in pybedtools for pergola data

--- test/pybedtools_pergola.py
+++ test/pybedtools_pergola.py
@@ -46,50 +46,39 @@ bed_tr2_food_fat = bed_str[('2', 'food_fat')]
 bedTools_tr2 = bed_tr2_food_fat.create_pybedtools()
 # print (bedTools_tr2)
 
-# pybedtools.helpers.set_bedtools_path("/Users/jespinosa/software/bedTools/bedtools2/bin/")
+# Subseting of bed file for testing meal merging
+c = pybedtools.BedTool(bedTools_tr2 [1:5])
+type (c)
+for i in c: print(i)
+
+############# Working command
+###### c_values = c_short.merge(d=100, stream=True, c=5, o="sum")
+# c_short = pybedtools.BedTool(bedTools_tr2 [1:5])
+# c_values_ok = c_short.merge(d=100, stream=True, c=5, o="sum")
+# for i in c_values_ok: print(i)
+
+################################
+# Meal modelling 120 s
+# mergeBed -i ${track} -d 120 -S + -c 4,5,6,9 -o distinct,sum,distinct,collapse -delim ";" > ${filename}_joined.bed
+# mergeBed -i tr_1_dt_food_sc.bed -d 120 -c 5 -o sum  > tr_merge.bed
+
+# bed short for testing
+b_short = pybedtools.BedTool(bedTools_tr2 [1:5])
+
+# si lo voy a leer también con pergola quiza no me haria falta que tuviera todos los campos
+# lo podria leer solo con los intervalos y los valores 
+# Algo así
+# b_values_test = b_short.merge(d=100, stream=True, c=(5), o=("sum"))
+
+# collapse (i.e., print a delimited list (duplicates allowed))
+# distinct the same no duplicates allowed
+# c_values_test = c_short.merge(d=100, stream=True, c=(4,5), o=("distinct", "sum"))
+b_values_test = b_short.merge(d=100, stream=True, c=(4,5,6,9), o=("distinct","sum","distinct","collapse"))
+
+type (b_values_test)
+
+for i in b_values_test: print(i)
 
 # Intersect example
 # c = bedTools_tr2.intersect(bedTools_tr1) 
 # print (c)
-
-# Meal modelling 120 s
-# mergeBed -i ${track} -d 120 -S + -c 4,5,6,9 -o distinct,sum,distinct,collapse -delim ";" > ${filename}_joined.bed
-c_short = pybedtools.BedTool(bedTools_tr2 [1:5])
-c = pybedtools.BedTool(bedTools_tr2 [1:5])
-for i in c_short: print(i)
-# for i in c: print(i)
-
-c_values = c_short.merge(d=100, stream=True, column=4)
-# c = c_short.merge(d=100, stream=True)
-
-# for i in c: print(i)
-for i in c_values: print(i)
-
-# fn = BedTool(iter(fn)).saveas().fn
-
-# a = pybedtools.BedTool(bed_tr1_food_sc, from_string=True)
-
-# # print (a)
-
-# bed_tr2_food_fat = bed_str[('2', 'food_fat')]
-# bed_file_single.save_track(name_file = "files_data")
-
-# # podria crear una funcion que hiciera un temporal dentro de bed
-# # este metodo solo seria llamado desde el metodo
-# # bed.create_pybedtools
-
-# bed_file_single.
-# f = NamedTemporaryFile(delete=False)
-
-# b = pybedtools.BedTool(bed_tr2_food_fat, from_string=True)
-# # print (b)
-# # c = a[2:4]
-
-# # intersecting
-# c = a.intersect(b) 
-# print (c)
-# # 
-
-
-
-
