7416e1ae2acfa7ad298173b2d996491ad7b8d58a
Jose Espinosa <kadomu@gmail.com>
2016-01-24 19:52:39 +0100

    Complement of merged meals implemented in the script

--- test/pybed_pergola_newFunction.py
+++ test/pybed_pergola_newFunction.py
@@ -9,7 +9,9 @@
 import pybedtools
 
 bedG=pybedtools.BedTool("/Users/jespinosa/git/pergola/test/shortPos.bedGraph")
-print bedG
+# print bedG
+
+w_dir = "/Users/jespinosa/git/pergola/test/"
 
 ##############
 # Here I try to create a BedTool Object using new_create_pybedtools
@@ -24,8 +26,12 @@ data_read = int_data.read(relative_coord=True)
 
 #####################
 ###### NUEVO
+# Generate a chr.size file in order to calculate complement of merged meals
+chr_file_n = "chrom"
+mapping.write_chr_sizes (data_read, path_w=w_dir, file_n=chr_file_n)
 
-
+chr_file = w_dir + chr_file_n + ".sizes"
+print "*******************", chr_file
 
 data_type_col = {'food_sc': 'orange', 'food_fat':'blue'}
 
@@ -45,5 +51,21 @@ type (bed_tr1_food_sc)
 bedTools_tr1 = bed_tr1_food_sc.new_create_pybedtools()
 type(bedTools_tr1)
 
-for i in bedTools_tr1: print(i)
+# for i in bedTools_tr1: print(i)
 
+dict_bed_merged = {}
+
+#############
+# For each track merge meals
+for tr, bed in bed_str.iteritems():
+# for bed in bed_str:
+    bed_BedTools = bed.new_create_pybedtools()
+    bed_merged = bed_BedTools.merge(d=120, stream=True, c=(4,5,6,9), o=("distinct","sum","distinct","collapse"))
+    # dict_bed_merged[tr] = bed_merged
+    # print bed_merged
+    # '.join(tup)
+    # bed_merged.saveas(''.join(tr) + "upstream_regions.bed")
+    ## COMPLEMENT
+    bed_merged_comp = bed_merged.complement(g=chr_file)
+    print bed_merged_comp.head(5) 
+    # dict_bed_merged[tr] = bed_merged
