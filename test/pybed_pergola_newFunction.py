460a99913c4cc38a9c5ae9322137f24460c63f46
Jose Espinosa <kadomu@gmail.com>
2016-01-24 23:48:48 +0100

    Adding bed objects for phases to intersect files

--- test/pybed_pergola_newFunction.py
+++ test/pybed_pergola_newFunction.py
@@ -22,6 +22,18 @@ mapping_data = mapping.MappingInfo("/Users/jespinosa/git/pergola/sample_data/fee
 
 int_data = intervals.IntData("/Users/jespinosa/git/pergola/sample_data/feeding_behavior/feedingBehavior_HF_mice.csv", map_dict=mapping_data.correspondence)
 
+###################
+# Write phases file 
+# phases_light.bed
+# phases_dark.bed
+light_ph_f = w_dir + "phases_light.bed"
+dark_ph_f = w_dir + "phases_dark.bed"
+light_bed = pybedtools.BedTool(light_ph_f)
+dark_bed = pybedtools.BedTool(dark_ph_f)
+
+# type (data_read)
+mapping.write_cytoband(int_data, end=int_data.max)
+
 data_read = int_data.read(relative_coord=True)
 
 #####################
@@ -32,6 +44,7 @@ mapping.write_chr_sizes (data_read, path_w=w_dir, file_n=chr_file_n)
 
 chr_file = w_dir + chr_file_n + ".sizes"
 print "*******************", chr_file
+
 
 data_type_col = {'food_sc': 'orange', 'food_fat':'blue'}
 
