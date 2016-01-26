43b252bf34b443a0af22760b0e9393e243a55c4e
Jose Espinosa <kadomu@gmail.com>
2016-01-26 15:25:15 +0100

    Reading experimental phases file from sample_data folder in the repo

--- test/pybed_pergola_newFunction.py
+++ test/pybed_pergola_newFunction.py
@@ -3,30 +3,28 @@
 ################################################################
 ### Jose A Espinosa. CSN/CB-CRG Group. Jan 2016              ###
 ################################################################
-### Script creating pybedtools objects directly from pergola ###
 ### Script creates BedTools objects from a file containing   ###
 ### mice feeding behavior and uses tools from pybedtools to  ###
 ### extract intermeals intervals (complement) and intersect  ###
 ### them with day and experimental phases.                   ###
 ### Generates a bed file for each track with the result of   ###
-### the above described operations. 
+### the above described operations.                          ###
 ################################################################
 
 import pybedtools
+from os import path, getcwd
 
-bedG=pybedtools.BedTool("/Users/jespinosa/git/pergola/test/shortPos.bedGraph")
-# print bedG
-
-w_dir = "/Users/jespinosa/git/pergola/test/"
+base_dir = path.dirname(getcwd())
+out_dir = base_dir + "/test/"
 
 ##############
 # Here I try to create a BedTool Object using new_create_pybedtools
 from pergola import mapping
 from pergola import intervals
 
-mapping_data = mapping.MappingInfo("/Users/jespinosa/git/pergola/sample_data/feeding_behavior/b2g.txt")
+mapping_data = mapping.MappingInfo(base_dir + "/sample_data/feeding_behavior/b2g.txt")
 
-int_data = intervals.IntData("/Users/jespinosa/git/pergola/sample_data/feeding_behavior/feedingBehavior_HF_mice.csv", map_dict=mapping_data.correspondence)
+int_data = intervals.IntData(base_dir + "/sample_data/feeding_behavior/feedingBehavior_HF_mice.csv", map_dict=mapping_data.correspondence)
 
 data_read = int_data.read(relative_coord=True)
 
@@ -38,31 +36,40 @@ data_read = int_data.read(relative_coord=True)
 # Write phases file
 mapping.write_cytoband(int_data, end = int_data.max - int_data.min, delta=43200, start_phase="dark")
 
-light_ph_f = w_dir + "phases_light.bed"
-dark_ph_f = w_dir + "phases_dark.bed"
+light_ph_f = out_dir +  "phases_light.bed"
+dark_ph_f = out_dir + "phases_dark.bed"
 light_bed = pybedtools.BedTool(light_ph_f)
 dark_bed = pybedtools.BedTool(dark_ph_f)
 
-#####################
-###### NUEVO
 # Generate a chr.size file in order to calculate complement of merged meals
 chr_file_n = "chrom"
-mapping.write_chr_sizes (data_read, path_w=w_dir, file_n=chr_file_n)
-chr_file = w_dir + chr_file_n + ".sizes"
+mapping.write_chr_sizes (data_read, path_w=out_dir, file_n=chr_file_n)
+chr_file = out_dir + chr_file_n + ".sizes"
 
-# Dictionary of colors
+# Dictionary to set colors of each type of food
 data_type_col = {'food_sc': 'orange', 'food_fat':'blue'}
 
 bed_str = data_read.convert(mode="bed", data_types=["food_sc", "food_fat"], data_types_actions="all", color_restrictions=data_type_col)
-mapping_bed = mapping.MappingInfo("/Users/jespinosa/git/pergola/test/pybed2perg.txt")
+mapping_bed = mapping.MappingInfo(out_dir + "/pybed2perg.txt")
 
-# Habituation phase
-hab_bed = pybedtools.BedTool("/Users/jespinosa/phecomp/20140807_pergola/bedtools_ex/intermeal_duration/data/exp_phases_hab.bed")
-# Development phase
-dev_bed = pybedtools.BedTool("/Users/jespinosa/phecomp/20140807_pergola/bedtools_ex/intermeal_duration/data/exp_phases_dev.bed")
+# Experimentals phases
+# Habituation phase (before high-fat food introduction)
+mapping_file_data_exp_ph = mapping.MappingInfo(base_dir + "/sample_data/feeding_behavior/f2g.txt")
+exp_ph_data = intervals.IntData(base_dir + "/sample_data/feeding_behavior/phases_exp.csv", map_dict=mapping_file_data_exp_ph.correspondence)
+data_exp_ph_tr = exp_ph_data.read(relative_coord=True)  
+exp_ph_bed = data_exp_ph_tr.convert(mode="bed", dataTypes_actions="all", tracks_merge=exp_ph_data.tracks)
 
-#############
-# For each track merge meals
+hab_bed = exp_ph_bed [('1_2', 'Habituation phase')].new_create_pybedtools()
+dev_bed = exp_ph_bed [('1_2', 'Development phase')].new_create_pybedtools()
+
+
+# hab_bed = pybedtools.BedTool("/Users/jespinosa/phecomp/20140807_pergola/bedtools_ex/intermeal_duration/data/exp_phases_hab.bed")
+# # Development phase
+# dev_bed = pybedtools.BedTool("/Users/jespinosa/phecomp/20140807_pergola/bedtools_ex/intermeal_duration/data/exp_phases_dev.bed")
+
+# For each track merge feeding acts that are separated by less than 120 seconds (thus generating feeding bouts track)
+# Calculates the complement of feeding bouts (intermeal intervals) and intersect them with day and experimental phases
+# Dumping results into bed files
 for tr, bed in bed_str.iteritems():
     bed_BedTools = bed.new_create_pybedtools()
     bed_merged_fn = bed_BedTools.merge(d=120, stream=True, c=(4,5,6,9), o=("distinct","sum","distinct","collapse")).saveas().fn
@@ -79,4 +86,3 @@ for tr, bed in bed_str.iteritems():
     hab_dark_intermeals_bed = bed_merged_comp.intersect(hab_bed).intersect(dark_bed).saveas('tr_' + '_'.join(tr) + "_compl_hab_dark.bed")
     dev_light_intermeals_bed = bed_merged_comp.intersect(dev_bed).intersect(light_bed).saveas('tr_' + '_'.join(tr) + "_compl_dev_light.bed")
     dev_dark_intermeals_bed = bed_merged_comp.intersect(dev_bed).intersect(dark_bed).saveas('tr_' + '_'.join(tr) + "_compl_dev_dark.bed")
-                                                                                           
