50a44bd43cabc04c0c51c5b75805d1f764b0c399
Jose Espinosa <kadomu@gmail.com>
2016-04-25 11:56:34 +0200

    GPLv3 license add on top of all the main pergola scripts

--- pergola/tracks.py
+++ pergola/tracks.py
@@ -1,3 +1,21 @@
+#  Copyright (c) 2014-2016, Centre for Genomic Regulation (CRG).
+#  Copyright (c) 2014-2016, Jose Espinosa-Carrasco and the respective authors.
+#
+#  This file is part of Pergola.
+#
+#  Pergola is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pergola is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Pergola.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 ======================
 Module: pergola.tracks
@@ -18,8 +36,8 @@ data as an iterator and the attributes generated when reading file
 :py:class:`~pergola.tracks.Bed` :class:`~pergola.tracks.GenomicContainer` class 
 for Bed files.
 
-:py:class:`~pergola.tracks.BedGraph` :class:`~pergola.tracks.GenomicContainer` class 
-for BedGraph files.
+:py:class:`~pergola.tracks.BedGraph` :class:`~pergola.tracks.GenomicContainer` 
+class for BedGraph files.
 
 """
 
