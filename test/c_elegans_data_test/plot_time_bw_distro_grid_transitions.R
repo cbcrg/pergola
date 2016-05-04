4be3b46da53fcdfdfab8fc7d3290ff67cb0be1c6
Jose Espinosa <kadomu@gmail.com>
2016-05-04 19:33:35 +0200

    Adding GPL 3 license on top of scripts

--- test/c_elegans_data_test/plot_time_bw_distro_grid_transitions.R
+++ test/c_elegans_data_test/plot_time_bw_distro_grid_transitions.R
@@ -1,8 +1,25 @@
+#  Copyright (c) 2014-2016, Centre for Genomic Regulation (CRG).
+#  Copyright (c) 2014-2016, Jose Espinosa-Carrasco and the respective authors.
+#
+#  This file is part of Pergola.
+#
+#  Pergola is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pergola is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Pergola.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 ### Jose A Espinosa. NPMMD/CB-CRG Group. Apr 2015                         ###
 #############################################################################
 ### Mean values for each group of worms                                   ###
-### Using bed files raw data intercepted with the motion                  ### 
+### Using bed files raw data to calculate time between motion periods     ### 
 #############################################################################
 
 # To use this script in ant first export this:
