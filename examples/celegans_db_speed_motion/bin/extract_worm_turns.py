8a58f27717753533777ae32efa25f4b627d83358
Jose Espinosa <kadomu@gmail.com>
2016-06-07 10:39:25 +0200

    Adding GPL3 license statement to files

--- examples/celegans_db_speed_motion/bin/extract_worm_turns.py
+++ examples/celegans_db_speed_motion/bin/extract_worm_turns.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+#  Copyright (c) 2014-2016, Centre for Genomic Regulation (CRG).
+#  Copyright (c) 2014-2016, Jose Espinosa-Carrasco and the respective authors.
+#
+#  This file is part of Pergola.
+#
+#  Pergola is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pergola is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Pergola.  If not, see <http://www.gnu.org/licenses/>.
+
 # Loading libraries
 from argparse import ArgumentParser
 from scipy.io  import loadmat
