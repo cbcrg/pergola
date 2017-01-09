b74de425180619869072e02da8bd3a3943cd9b35
Jose Espinosa <kadomu@gmail.com>
2017-01-09 10:00:04 +0100

    Importing new jaaba_parsers module

--- test/jaaba2pergola_dev.py
+++ test/jaaba2pergola_dev.py
@@ -21,20 +21,21 @@ from numpy    import mean#parsers
 # from pergola  import mapping#parsers
 # from pergola  import intervals#parsers
 from pergola  import parsers
+from pergola import jaaba_parsers
 # from tempfile import NamedTemporaryFile#parsers
 
 # This part has been included in the function
 # jaaba_data = io.loadmat('/Users/jespinosa/JAABA_MAC_0.5.1/sampledata/Chase1_TrpA_Rig1Plate15BowlA_20120404T141155/scores_chase.mat')
 
 
-parsers.jaaba_scores_to_csv(input_file='/Users/jespinosa/JAABA_MAC_0.5.1/sampledata_v0.5/Chase1_TrpA_Rig1Plate15BowlA_20120404T141155/scores_chase.mat', 
+jaaba_parsers.jaaba_scores_to_csv(input_file='/Users/jespinosa/JAABA_MAC_0.5.1/sampledata_v0.5/Chase1_TrpA_Rig1Plate15BowlA_20120404T141155/scores_chase.mat', 
                             path_w='/Users/jespinosa/git/pergola/test', norm=True, data_type="chase")
 
 print ("You correctly transform your JAABA scores into a csv file")
 
 input_jaaba_file = '/Users/jespinosa/JAABA_MAC_0.5.1/sampledata_v0.5/Chase1_TrpA_Rig1Plate15BowlA_20120404T141155/scores_chase.mat'
 map_file_jaaba = "/Users/jespinosa/git/pergola/test/jaaba2pergola.txt"
-int_data_jaaba = parsers.jaaba_scores_to_intData(input_file = input_jaaba_file, map_jaaba = map_file_jaaba, norm=True, data_type="chase")
+int_data_jaaba = jaaba_parsers.jaaba_scores_to_intData(input_file = input_jaaba_file, map_jaaba = map_file_jaaba, norm=True, data_type="chase")
 
 print int_data_jaaba.min
 print int_data_jaaba.max
