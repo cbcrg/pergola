2c9e93b4be8ef76f3b2df6aca04d209cfdb1ad4d
Jose Espinosa <kadomu@gmail.com>
2017-06-21 11:33:23 +0200

    Modifying setup.py to check for dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ from os import path
 
 PATH = path.abspath(path.split(path.realpath(__file__))[0])
 
-# "Operating System :: Microsoft :: Windows",
 TAGS = [
     "Development Status :: 2 - Pre-Alpha",
     "Environment :: Console",
@@ -35,34 +34,102 @@ TAGS = [
     "Natural Language :: English",
     "Operating System :: MacOS",
     "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
     "Programming Language :: Python",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Topic :: Scientific/Engineering :: Information Analysis",
     "Topic :: Scientific/Engineering :: Visualization",
     "Topic :: Software Development :: Libraries :: Python Modules",
     ]
 
-setup(name='pergola',
-      version='0.1',
-      description='A library to analyze and visualize behavioral data by unlocking genomic tools ',
-      long_description= open('README.md').read(),
-      url='http://github.com/cbcrg/pergola',
-      author='Jose Espinosa-Carrasco',
-      author_email='espinosacarrascoj@gmail.com',
-      license='GNU General Public License 3.0',
-      package_dir = {'pergola': PATH + '/pergola', 'scripts': PATH + '/pergola/scripts',},
-      package_data={'mypkg': ['data/sample_data/*.csv']},
-      packages=['pergola', 'scripts'], 
-#       scripts = ['scripts/pergola_rules.py', 'scripts/pergola_isatab.py', 'scripts/jaaba_to_pergola.py'],      
-      zip_safe=False,
-      classifiers = TAGS,
-#       install_requires=["scipy>=0.17", "pybedtools>=0.7", "numpy>=1.10"],           
-      entry_points={
-        'console_scripts': [
-            'pergola = scripts.pergola_rules:main',
-            'pergola_rules.py = scripts.pergola_rules:main',            
-            'jaaba_to_pergola = scripts.jaaba_to_pergola:main',  
-            'pergola_isatab.py = scripts.pergola_isatab:main',            
-        ]}
-      )
 
-# mandatory package to add are argparse
+def can_import(modname):
+    """
+    Test whether a module can be imported (already installed in the system)
+    
+    """
+
+    try:
+        __import__(modname)
+    except ImportError:
+        return None
+    else:
+        return True
+
+
+def ask(string, valid_values, default=-1, case_sensitive=False):
+    """ 
+    Asks for a keyborad answer 
+    
+    """
+
+    v = None
+    if not case_sensitive:
+        valid_values = [value.lower() for value in valid_values]
+
+    while v not in valid_values:
+        v = raw_input("%s [%s]" % (string,','.join(valid_values)))
+
+        if v == '' and default >= 0:
+            v = valid_values[default]
+        if not case_sensitive:
+            v = v.lower()
+
+    return v
+
+
+PYTHON_DEPENDENCIES = [
+    ["csv"       , "Required for reading csv input files.", 1],
+    ["argparse"  , "Required to read scripts arguments", 1],
+    ["numpy"     , "Required for reading arrays from Jaaba matlab files and arange.", 1],
+    ["bcbio"     , "Required for reading isatab files, aka biopy-isatab", 0],
+    ["scipy"     , "Required for reading Jaaba matlab files.", 0],
+    ["pybedtools", "Required to create pybedtools objects from Bed, BedGraph and Gff pergola objects.", 0]]
+
+
+print "Checking dependencies..."
+missing = False
+for mname, msg, ex in PYTHON_DEPENDENCIES:
+
+    if not can_import(mname):
+        print "  *", mname, "cannot be found in your python installation."
+        print "   ->", msg
+
+        if ex:
+            missing = True
+        else:
+            print('\nWARNING: It is HIGHLY RECOMMENDED to install all Pergola ' +
+                  'dependencies.\nThe installation will continue. try to fix '
+                  'it afterwards.')
+
+if missing:
+    exit("Essential dependencies missing, please review and install.\n")
+
+
+def main():
+    setup(name='pergola',
+          version='0.1',
+          description='A library to analyze and visualize behavioral data by unlocking genomic tools',
+          long_description=open('README.md').read(),
+          url='http://github.com/cbcrg/pergola',
+          author='Jose Espinosa-Carrasco',
+          author_email='espinosacarrascoj@gmail.com',
+          license='GNU General Public License 3.0',
+          package_dir={'pergola': PATH + '/pergola', 'scripts': PATH + '/pergola/scripts', },
+          package_data={'mypkg': ['data/sample_data/*.csv']},
+          packages=['pergola', 'scripts'],
+          #       scripts = ['scripts/pergola_rules.py', 'scripts/pergola_isatab.py', 'scripts/jaaba_to_pergola.py'],
+          zip_safe=False,
+          classifiers=TAGS,
+          #       install_requires=["scipy>=0.17", "pybedtools>=0.7", "numpy>=1.10"],
+          entry_points={
+              'console_scripts': [
+                  'pergola = scripts.pergola_rules:main',
+                  'pergola_rules.py = scripts.pergola_rules:main',
+                  'jaaba_to_pergola = scripts.jaaba_to_pergola:main',
+                  'pergola_isatab.py = scripts.pergola_isatab:main',
+              ]}
+          )
+
+if __name__ == '__main__':
+    exit(main())
